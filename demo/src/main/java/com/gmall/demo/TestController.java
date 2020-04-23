a1a4128324d634a6da439f70c257d1b60e9f3a56
tianyitq <1120462793@qq.com>
2020-04-23 22:14:53 +0800

    v1.0

---
+++ demo/src/main/java/com/gmall/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.gmall.demo;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author ：mmzs
+ * @date ：Created in 2020/4/23 22:12
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Controller
+public class TestController {
+
+}
